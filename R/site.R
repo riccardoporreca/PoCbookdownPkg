fd71381a8abe5fe66fda8933e18c9525381c2b52
fvitalini <francesca.vitalini@mirai-solutions.com>
2019-09-03 18:20:28 +0200

    test with different output_dir

--- R/site.R
+++ R/site.R
@@ -6,11 +6,11 @@
 #' @param ... additional arguments to be passed to bookdown::render_book .
 #'
 #' @export
-render_site <- function(..., output_dir = "_site") {
+render_site <- function(..., output_dir = '_site') {
   # absolute path of output_dir (which must exist)
   dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
-  output_dir <- normalizePath(output_dir)
-  input_dir <- system.file("site", package = "PoCbookdownPkg")
+  # output_dir <- normalizePath(output_dir)
+  input_dir <- getwd() #system.file('site', package = 'PoCbookdownPkg')
   restore_dir <- setwd(input_dir) # returns the wd before the call
   on.exit(setwd(restore_dir))
   bookdown::render_book(".", output_dir = output_dir, ...)
