1c957082548d5bbe1c3ab41107fafaf9ef4f7db9
fvitalini <francesca.vitalini@mirai-solutions.com>
2019-09-03 18:25:42 +0200

    test travis.yml changes

--- R/site.R
+++ R/site.R
@@ -8,10 +8,10 @@
 #' @export
 render_site <- function(..., output_dir = '_site') {
   # absolute path of output_dir (which must exist)
-  dir.create(output_dir, recursive = TRUE, showWarnings = FALSE)
+  dir.create("./_site", recursive = TRUE, showWarnings = FALSE)
   # output_dir <- normalizePath(output_dir)
-  input_dir <- getwd() #system.file('site', package = 'PoCbookdownPkg')
-  restore_dir <- setwd(input_dir) # returns the wd before the call
-  on.exit(setwd(restore_dir))
-  bookdown::render_book(".", output_dir = output_dir, ...)
+  input_dir <- system.file('site', package = 'PoCbookdownPkg')
+  #restore_dir <- setwd(input_dir) # returns the wd before the call
+  #on.exit(setwd(restore_dir))
+  bookdown::render_book(".", output_dir = "./_site", ...)
 }
